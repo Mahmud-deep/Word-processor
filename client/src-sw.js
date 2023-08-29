4ddbcbf47176fea13e580593bae3e9b821994d5e
mahmud khan deep <mahmudkhan_deep@yahoo.com>
2023-08-28 22:49:21 -0400

    modified

--- client/src-sw.js
+++ client/src-sw.js
@@ -1,5 +1,5 @@
 const { offlineFallback, warmStrategyCache } = require('workbox-recipes');
-const { CacheFirst } = require('workbox-strategies');
+const { CacheFirst, StaleWhileRevalidate } = require('workbox-strategies');
 const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
@@ -26,10 +26,15 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// DONE: Implement asset caching
-registerRoute(({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+// TODO: Implement asset caching - DONE
+registerRoute(
+  ({ request }) => ["style", "script", "worker"].includes(request.destination),
   new StaleWhileRevalidate({
-    cacheName: 'asset-cache',
-    plugins: [new CacheableResponsePlugin({ statuses: [0, 200] })]
+    cacheName: "asset-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
   })
 );
